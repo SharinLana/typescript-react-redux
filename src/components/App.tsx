41ef3ed6babf4975c76bd2b739afc068719bdf77
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-29 15:50:41 -0700

    use RepositoriesList component inside of the App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import RepositoriesList from "./RepositoriesList"
 
 function App() {
   return (
     <div >
-      
+      <h2>Search For a Package</h2>
+      <RepositoriesList />
     </div>
   );
 }
