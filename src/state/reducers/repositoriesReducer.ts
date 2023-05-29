6215095255aa2832adb7b45236b9be5ad57e5d2a
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-29 13:14:51 -0700

    create an interface Action to set the action types

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -3,7 +3,13 @@ interface RepositoriesState {
   error: string | null;
   data: string[];
 }
-const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
+
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
   switch (action.type) {
     case "search_repositories":
       return { loading: true, error: null, data: [] };
