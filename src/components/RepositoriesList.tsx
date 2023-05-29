ad95244045eb1785421bb4a4b88306577e79d820
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-29 16:44:50 -0700

    invoke searchRepositories action creator in the formSubmitHandler and passed the input value into it

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
 
   const formSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
+    searchRepositories(term);
   };
 
   return (
