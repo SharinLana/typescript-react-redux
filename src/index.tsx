ccf74d31fd3c758f3f11866f242ccfe4f6d3a03a
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-29 15:47:07 -0700

    wrapped the App component in the react-redux Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { store } from "./state";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
