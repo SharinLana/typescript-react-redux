fa1c222c3de55a50d505d539d80b7639566254bf
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-29 12:53:41 -0700

    add returned values to each case in the repositoriesReducer.ts

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,13 +1,16 @@
 interface repositoriesState {
   loading: boolean;
   error: string | null;
-  data: string[]
+  data: string[];
 }
 const reducer = (state: repositoriesState, action: any) => {
   switch (action.type) {
-    case 'search_repositories':
-    case 'search_repositories_success':
-    case 'search_repositories_error':
+    case "search_repositories":
+      return { loading: true, error: null, data: [] };
+    case "search_repositories_success":
+      return { loading: false, error: null, data: action.payload };
+    case "search_repositories_error":
+      return { loading: false, error: action.payload, data: [] };
 
     default:
       return state;
