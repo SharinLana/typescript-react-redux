53bbb5c45e210f1272cebc63d58e18eaf9a80f07
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-29 17:45:45 -0700

    rendered the received state properties conditionally

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "../hooks/useActions";
+import RepoList from "./RepoList";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
-  const { loading, error, data } = useTypedSelector((state) => state.repositories);
+  const { loading, error, data } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const formSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -13,10 +16,17 @@ const RepositoriesList: React.FC = () => {
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <input value={term} onChange={(e) => setTerm(e.target.value)} />
-      <button>Search</button>
-    </form>
+    <>
+      <form onSubmit={formSubmitHandler}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+      <div>
+        {error && <h3>{error}</h3>}
+        {loading && <h3>Loading...</h3>}
+        {!error && !loading && <RepoList data={data} />}
+      </div>
+    </>
   );
 };
 
