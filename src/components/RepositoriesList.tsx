aa940ba9b4c6ffe09251b96037cca716a4c1fe4d
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-29 17:12:16 -0700

    receive state from repositoriesReducer in the RepositoriesList

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { loading, error, data } = useSelector((state: any) => state);
 
   const formSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
